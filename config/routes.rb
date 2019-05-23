ae6a12adf91272b1db2f8ea3d55695b5c600ed17
Nikos Agathos <nicolas.agathos@gmail.com>
2019-05-23 16:53:24 +0200

    added Cocktail controller and the appropriate routes for the specs of this exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses
+      # resources :ingredients
+    # end
+  end
+  resources :doses
+  # resources :doses do
+  #   resources :ingredients
+  # end
+
+  # resources :cocktails do
+  #   resources :ingredients
+  # end
 end
